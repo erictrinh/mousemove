830398e60d11847c83e44d09ae98e37024b56fdc
Eric Trinh <et.trinity@gmail.com>
2014-01-08 22:35:37 -0600

    Initial Commit

---
+++ mousemove/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mousemove
+//
+//  Created by Eric Trinh on 1/8/14.
+//  Copyright (c) 2014 Eric Trinh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
